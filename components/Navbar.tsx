3c59cf6e579aaa15e30558c8a7caab97e0de23b1
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-11-02 07:55:46 +0900

    updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,26 @@
+import { BsPeople } from "react-icons/bs";
 import Link from "next/link";
 import React from "react";
 import MobileMenu from "./MobileMenu";
 import Image from "next/image";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { BookAIcon, GroupIcon, HomeIcon, LogIn, UsersRoundIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
       {/* LEFT */}
       <div className="md:hidden lg:block w-[20%]">
-        <Link href={"/"} className="text-xl font-extrabold">
+        <Link
+          href={"/"}
+          className="text-xl md:text-4xl font-extrabold tracking-tighter"
+        >
           SOCIA<span className="text-primary">LY</span>
         </Link>
       </div>
@@ -16,41 +28,68 @@ const Navbar = () => {
       {/* CENTER */}
       <div className="hidden md:flex w-[50%] text-sm">
         <div className="flex gap-6 text-gray-600">
-          <Link href={"/"} className="flex gap-2 items-center"></Link>
-          <Image
-            src={"/home.png"}
-            alt="Homepage"
-            width={16}
-            height={16}
-            className="w-4 h-4"
-          />
-          <span>Homepage</span>
-
-          <Link href={"/"} className="flex gap-2 items-center"></Link>
-          <Image
-            src={"/friends.png"}
-            alt="Friends"
-            width={16}
-            height={16}
-            className="w-4 h-4"
-          />
-          <span>Friends</span>
-
-          <Link href={"/"} className="flex gap-2 items-center"></Link>
-          <Image
-            src={"/stories.png"}
-            alt="Stories"
-            width={16}
-            height={16}
-            className="w-4 h-4"
-          />
-          <span>Stories</span>
+          <Link href={"/"} className="flex gap-2 items-center">
+            <HomeIcon />
+            <span>Homepage</span>
+          </Link>
+          <Link href={"/friends"} className="flex gap-2 items-center">
+            <UsersRoundIcon/>
+            <span>Friends</span>
+          </Link>
+          <Link href={"/stories"} className="flex gap-2 items-center">
+            <BookAIcon />
+            <span>Stories</span>
+          </Link>
         </div>
       </div>
 
       {/* RIGHT */}
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
-        <MobileMenu />
+        <ClerkLoading>
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        </ClerkLoading>
+
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="cursor-pointer">
+              <Image src={"/people.png"} alt="People" width={20} height={20} />
+            </div>
+            <div className="cursor-pointer">
+              <Image
+                src={"/messages.png"}
+                alt="Messages"
+                width={20}
+                height={20}
+              />
+            </div>
+            <div className="cursor-pointer">
+              <Image
+                src={"/notifications.png"}
+                alt="Notifications"
+                width={20}
+                height={20}
+              />
+            </div>
+            <UserButton />
+            <MobileMenu />
+          </SignedIn>
+
+          <SignedOut>
+            <div className="flex gap-2 items-center">
+              <LogIn className="h-4 w-4" />
+              <Link href={"/sign-in"} className="text-sm">
+                Login/Register
+              </Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
       </div>
     </div>
   );
